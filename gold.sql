a3b5e7f52bf48c498a39ab331225cd4665974846
HG <tuankietin2005@gmail.com>
2025-05-02 04:30:56 +0700

    Update gold.sql

--- gold.sql
+++ gold.sql
@@ -54,3 +54,34 @@ SELECT DISTINCT
     END AS WeekType
 FROM silver_crm_sale s
 ORDER BY s.sls_date;
+
+/* fact table */
+CREATE TABLE fact_table (
+    sale_id INT IDENTITY(1,1) PRIMARY KEY,
+    channel_id INT,
+    country_id INT,
+    date_id INT,
+    model_id INT,
+    quantity_sold INT,
+    revenue DECIMAL(18,2),
+    tm_Year INT,
+    FOREIGN KEY (channel_id) REFERENCES dim_channel(channel_id),
+    FOREIGN KEY (country_id) REFERENCES dim_country(country_id),
+    FOREIGN KEY (date_id) REFERENCES dim_date(date_id),
+    FOREIGN KEY (model_id) REFERENCES dim_model(model_id)
+);
+
+INSERT INTO fact_table (channel_id, country_id, date_id, model_id, quantity_sold, revenue, tm_Year)
+SELECT 
+    dc.channel_id,
+    dco.country_id,
+    dd.date_id,
+    dm.model_id,
+    s.sls_quantity_sold,
+    s.sls_revenue,
+    s.sls_year
+FROM silver_crm_sale s
+JOIN dim_channel dc ON s.sls_channel = dc.channel
+JOIN dim_country dco ON s.sls_country = dco.country AND s.sls_region = dco.region
+JOIN dim_date dd ON s.sls_date = dd.time_date
+JOIN dim_model dm ON s.sls_model = dm.model;
