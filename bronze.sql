f7e3c51c51d5357d8077f946a04e2beb0942755e
HG <tuankietin2005@gmail.com>
2025-05-02 03:17:41 +0700

    Update bronze.sql

--- bronze.sql
+++ bronze.sql
@@ -12,3 +12,31 @@ CREATE TABLE bronze_crm_sale (
 	sls_country NVARCHAR(50),
 	sls_channel NVARCHAR(50)
 );
+
+/* images */
+CREATE TABLE image (
+	model NVARCHAR(50),
+	img NVARCHAR(1000)
+);
+BULK INSERT image
+FROM 'D:\BMW Files-20250501T172756Z-001\BMW Files\Car_Images.csv'
+WITH (
+	FIRSTROW = 2,
+	FIELDTERMINATOR = ',',
+	TABLOCK
+);
+
+// flag
+CREATE TABLE flag (
+	country NVARCHAR(50),
+	country_code NVARCHAR(50),
+	region NVARCHAR(50),
+	flag NVARCHAR(500)
+);
+BULK INSERT flag
+FROM 'D:\BMW Files-20250501T172756Z-001\BMW Files\Flags_URL.csv'
+WITH (
+	FIRSTROW = 2,
+	FIELDTERMINATOR = ',',
+	TABLOCK
+);
