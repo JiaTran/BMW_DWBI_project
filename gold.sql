a467de2f1e7e4829a9438a0a75a9dbe602b3bc7c
HG <tuankietin2005@gmail.com>
2025-05-02 03:43:02 +0700

    Update gold.sql

--- gold.sql
+++ gold.sql
@@ -26,3 +26,31 @@ CREATE TABLE dim_channel (
 
 INSERT INTO dim_channel(channel)
 SELECT DISTINCT sls_channel FROM silver_crm_sale
+
+CREATE TABLE dim_date (
+	date_id INT IDENTITY(1,1) PRIMARY KEY,
+	time_date DATE,
+	time_year INT,
+	time_month VARCHAR(10),
+	time_monthnum INT,
+	time_weekday VARCHAR(10),
+	time_weeknum INT,
+	time_qtr VARCHAR(5),
+	time_weektype VARCHAR(10)
+);
+/* dim_date */
+INSERT INTO dim_date ( time_date, time_year, time_month, time_monthnum, time_weekday, time_weeknum, time_qtr, time_weektype)
+SELECT DISTINCT
+	s.sls_date AS Date,
+    YEAR(s.sls_date) AS time_year,
+    DATENAME(MONTH, s.sls_date) AS time_month,
+    MONTH(s.sls_date) AS Monthnum,
+    DATENAME(WEEKDAY, s.sls_date) AS time_weekday,
+    DATEPART(WEEK, s.sls_date) AS time_weeknum,
+    'Q-' + CAST(DATEPART(QUARTER, s.sls_date) AS VARCHAR(1)) AS time_qtr,
+    CASE 
+        WHEN DATEPART(WEEKDAY, s.sls_date) IN (1, 7) THEN 'weekend'
+        ELSE 'weekday'
+    END AS WeekType
+FROM silver_crm_sale s
+ORDER BY s.sls_date;
